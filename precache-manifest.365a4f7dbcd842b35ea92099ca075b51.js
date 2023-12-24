274d6a4b1edce48791921c46218dc27426ef3196
SaipraveenSPK <saipraveen.annauniv@gmail.com>
2023-12-24 19:48:11 +0530

    Updates

---
+++ precache-manifest.365a4f7dbcd842b35ea92099ca075b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faa479a25879b945de9eafc26e73f41",
+    "url": "/Space-Whiz_E-Commerce-Website/index.html"
+  },
+  {
+    "revision": "b5e7eceb8840630847dd",
+    "url": "/Space-Whiz_E-Commerce-Website/static/css/2.fde8feac.chunk.css"
+  },
+  {
+    "revision": "7b9545108300985f5e20",
+    "url": "/Space-Whiz_E-Commerce-Website/static/css/main.cad6b8f2.chunk.css"
+  },
+  {
+    "revision": "b5e7eceb8840630847dd",
+    "url": "/Space-Whiz_E-Commerce-Website/static/js/2.a11bd671.chunk.js"
+  },
+  {
+    "revision": "7b9545108300985f5e20",
+    "url": "/Space-Whiz_E-Commerce-Website/static/js/main.1c6b17e6.chunk.js"
+  },
+  {
+    "revision": "eedad0b51fa40c9a6928",
+    "url": "/Space-Whiz_E-Commerce-Website/static/js/runtime~main.51f0a602.js"
+  }
+]);
