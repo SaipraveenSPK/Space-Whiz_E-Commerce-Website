79903936633c2d9d491e1a983bb5a37f6a777992
SaipraveenSPK <saipraveen.annauniv@gmail.com>
2023-12-24 22:16:05 +0530

    Updates

---
+++ precache-manifest.62a80350c2255738854461c841425881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff35e1f61862df35c27925b18a113272",
+    "url": "/Space-Whiz_E-Commerce-Website/index.html"
+  },
+  {
+    "revision": "b5e7eceb8840630847dd",
+    "url": "/Space-Whiz_E-Commerce-Website/static/css/2.fde8feac.chunk.css"
+  },
+  {
+    "revision": "34da77c5c80e6f33df62",
+    "url": "/Space-Whiz_E-Commerce-Website/static/css/main.cad6b8f2.chunk.css"
+  },
+  {
+    "revision": "b5e7eceb8840630847dd",
+    "url": "/Space-Whiz_E-Commerce-Website/static/js/2.a11bd671.chunk.js"
+  },
+  {
+    "revision": "34da77c5c80e6f33df62",
+    "url": "/Space-Whiz_E-Commerce-Website/static/js/main.358761a2.chunk.js"
+  },
+  {
+    "revision": "eedad0b51fa40c9a6928",
+    "url": "/Space-Whiz_E-Commerce-Website/static/js/runtime~main.51f0a602.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9c3693ee6f1ee1e1ced0a2b6732de06.js"
+  "/Space-Whiz_E-Commerce-Website/precache-manifest.62a80350c2255738854461c841425881.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Space-Whiz_E-Commerce-Website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
